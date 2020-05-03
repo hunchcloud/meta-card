603442af2b19146c567a03b354fbf95fe5238674
Ping Chen <remotenonsense@gmail.com>
2020-05-03 10:37:27 +0900

    v0.4.0 use query instead of path

--- src/meta-proxy.ts
+++ src/meta-proxy.ts
@@ -1,4 +1,4 @@
 const PROXY = (<any>window).META_PROXY || "https://meta-proxy.herokuapp.com";
 
 export const fetchMeta = async (url: string) =>
-  await (await fetch(`${PROXY}/${url}`)).json();
+  await (await fetch(`${PROXY}?q=${url}`)).json();
